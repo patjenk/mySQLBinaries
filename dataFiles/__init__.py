794767cf4b36db41d55adbdd120675e8164c7426
patjenk <me@patjenk.com>
2013-09-21 23:19:07 +0000

    Now we are reading the column names and column types out of the myisam format file.

--- dataFiles/__init__.py
+++ dataFiles/__init__.py
@@ -1,3 +1,38 @@
+from struct import unpack
+
+
+def enum(*sequential, **named):
+    """
+    Taken from http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+    """
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.iteritems())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+
+# Taken from include/mysql_com.h
+MYSQL_FIELD_TYPES = enum('MYSQL_TYPE_DECIMAL', 'MYSQL_TYPE_TINY',
+                        'MYSQL_TYPE_SHORT',  'MYSQL_TYPE_LONG',
+                        'MYSQL_TYPE_FLOAT',  'MYSQL_TYPE_DOUBLE',
+                        'MYSQL_TYPE_NULL',   'MYSQL_TYPE_TIMESTAMP',
+                        'MYSQL_TYPE_LONGLONG','MYSQL_TYPE_INT24',
+                        'MYSQL_TYPE_DATE',   'MYSQL_TYPE_TIME',
+                        'MYSQL_TYPE_DATETIME', 'MYSQL_TYPE_YEAR',
+                        'MYSQL_TYPE_NEWDATE', 'MYSQL_TYPE_VARCHAR',
+                        'MYSQL_TYPE_BIT',
+                        MYSQL_TYPE_NEWDECIMAL=246,
+                        MYSQL_TYPE_ENUM=247,
+                        MYSQL_TYPE_SET=248,
+                        MYSQL_TYPE_TINY_BLOB=249,
+                        MYSQL_TYPE_MEDIUM_BLOB=250,
+                        MYSQL_TYPE_LONG_BLOB=251,
+                        MYSQL_TYPE_BLOB=252,
+                        MYSQL_TYPE_VAR_STRING=253,
+                        MYSQL_TYPE_STRING=254,
+                        MYSQL_TYPE_GEOMETRY=255)
+
+
 class MyIsamFormatFileException(Exception):
     pass
 
@@ -10,7 +45,7 @@ class MyIsamFormatFile:
 
     def __init__(self, format_filename=None):
         self.format_filename = format_filename
-        self.file_handler = open(format_filename, "rb")
+        self.file_handler = open(self.format_filename, "rb")
         if not self.verify_header():
             raise MyIsamFormatFileException("Invalid Magic Number")
 
@@ -35,5 +70,42 @@ class MyIsamFormatFile:
         self.max_rows = header_string[18:22]
         self.min_rows = header_string[22:26]
         self.key_info_length = header_string[30:32]
-        self.mysql_version = header_string[51:55]
+        # Are the rest of these right?
+        self.mysql_version = header_string[50:54]
 
+        self.file_handler.seek(8238)
+        self.comment_length = ord(self.file_handler.read(1))
+        self.comment = self.file_handler.read(self.comment_length)
+
+        self.file_handler.seek(8450)
+        self.number_of_columns = unpack("<H", self.file_handler.read(2))[0]
+
+        self.file_handler.seek(8460)
+        self.n_length = unpack("<H", self.file_handler.read(2))[0]
+
+        self.file_handler.seek(8530)
+        self.column_names = []
+        for column_number in range(self.number_of_columns):
+            current_column_name_length = ord(self.file_handler.read(1))
+            self.column_names.append(self.file_handler.read(current_column_name_length-1))
+            self.file_handler.read(1) # Null terminator for string column name
+            self.file_handler.read(1) # Unknown Value 1
+            self.file_handler.read(1) # Unknown Value 2
+
+        # Wtf are these two columns? The documentation doesn't describe them well
+        self.number_of_bytes_in_a_column = ord(self.file_handler.read(1))
+        self.file_handler.read(1) # this is the same value as the previous byte
+
+        self.file_handler.read(4) # \00\02\00\00 Unknown according to the docs 
+
+        self.first_flags = self.file_handler.read(1)
+        self.second_flags = self.file_handler.read(1)
+
+        self.file_handler.read(3) # Not described by the docs. Probably left open for future changes
+
+        self.column_types = []
+        self.column_character_sets = []
+        for column_number in range(self.number_of_columns):
+            self.column_types.append(MYSQL_FIELD_TYPES.reverse_mapping[ord(self.file_handler.read(1))])
+            self.column_character_sets.append(ord(self.file_handler.read(1)))
+            self.file_handler.read(15) # unknown bytes
---
+++ dataFiles/tests/tests_myisam_format_file.py
@@ -0,0 +1,29 @@
+from .. import MyIsamFormatFile, MYSQL_FIELD_TYPES
+from unittest import TestCase
+
+
+class TestsReadMyIsamFormatFile(TestCase):
+    def test_verify(self):
+        MyIsamFormatFile("dataFiles/tests/examples/readme/myisam_table_one.frm")
+
+    def test_parse_header(self):
+        format_file = MyIsamFormatFile("dataFiles/tests/examples/readme/myisam_table_one.frm")
+        format_file.parse_header()
+        self.assertEqual(format_file.frm_ver, '\x09')
+        self.assertEqual(format_file.legacy_db_type, '\x03')
+        self.assertEqual(format_file.iosize, '\x00\x10')
+        self.assertEqual(format_file.length, '\x00\x30\x00\x00')
+        self.assertEqual(format_file.rec_length, '\x05\x04')
+        self.assertEqual(format_file.max_rows, '\x00\x00\x00\x00')
+        self.assertEqual(format_file.min_rows, '\x00\x00\x00\x00')
+        self.assertEqual(format_file.key_info_length, '\x09\x00')
+
+        self.assertEqual(format_file.number_of_columns, 5)
+        self.assertEqual(format_file.n_length, 43)
+        self.assertEqual(['PersonID', 'LastName', 'FirstName', 'Address', 'City'], format_file.column_names)
+        expected_column_types = [MYSQL_FIELD_TYPES.MYSQL_TYPE_LONG, MYSQL_FIELD_TYPES.MYSQL_TYPE_VARCHAR, MYSQL_FIELD_TYPES.MYSQL_TYPE_VARCHAR, MYSQL_FIELD_TYPES.MYSQL_TYPE_VARCHAR, MYSQL_FIELD_TYPES.MYSQL_TYPE_VARCHAR]
+        self.assertEqual(len(expected_column_types), len(format_file.column_types))
+        for column_number, expected_column_type in enumerate(expected_column_types):
+            print MYSQL_FIELD_TYPES.reverse_mapping[expected_column_type], format_file.column_types[column_number]
+            self.assertEqual(MYSQL_FIELD_TYPES.reverse_mapping[expected_column_type], format_file.column_types[column_number])
+        self.assertEqual([8, 8, 8, 8, 8], format_file.column_character_sets)
