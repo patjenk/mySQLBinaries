2c0edcb44cc59587c858ea5f82a711695c458a0d
patjenk <me@patjenk.com>
2014-09-30 15:41:39 +0000

    the test for reading and writing the myisam index file works. This is very very basic functionality and cannot do anything yet.

--- mySQLBinaries/dataFiles/myIsamIndexFile.py
+++ mySQLBinaries/dataFiles/myIsamIndexFile.py
@@ -14,6 +14,7 @@ class MyIsamIndexFile:
     def __init__(self, index_filename=None):
         self.index_filename = index_filename
         self.file_handler = open(self.index_filename, 'rb')
+        self.header_state = {}
         if not self.verify_header():
             raise MyIsamIndexFileException("Invalid Magic Number")
 
@@ -22,29 +23,42 @@ class MyIsamIndexFile:
         Return true if the file has the correct magic characters.
         """
         self.file_handler.seek(0)
+
+        # Why is this FE FE 07 01? The mysql source hardcodes this value in
+        # storage/myisam/mi_static.c. Is there a reason this number was
+        # chosen or was it arbitrary?
         return '\xfe\xfe\x07\x01' == self.file_handler.read(4)
 
 
     def parse_header(self):
         """
         Pull information out of the header of the format file.
-        """
-        self.file_handler.seek(0)
-        self.file_version = unpack("I", self.file_handler.read(4))
 
-        self.options = unpack("H", self.file_handler.read(2))
-        self.header_length = unpack("H", self.file_handler.read(2))
-        self.state_info_length = unpack("H", self.file_handler.read(2))
-        self.base_info_length = unpack("H", self.file_handler.read(2))
-        self.base_pos = unpack("H", self.file_handler.read(2))
+        The header of the index file has 4 sections.
+        state: occurs once
+        base: occurs once
+        keydef (including keysegs): occurs once for each key
+        recinfo: occurs once for each field
+        """
+
+        # Handle the state section of the header.
+        self.file_handler.seek(0)
+
+        self.header_state['file_version'] = unpack("I", self.file_handler.read(4))[0]
+        self.header_state['options'] = unpack("H", self.file_handler.read(2))[0]
+        self.header_state['length'] = unpack("H", self.file_handler.read(2))[0]
+        self.header_state['state_info_length'] = unpack("H", self.file_handler.read(2))[0]
+        self.header_state['base_info_length'] = unpack("H", self.file_handler.read(2))[0]
+        self.header_state['base_pos'] = unpack("H", self.file_handler.read(2))[0]
+
         self.key_parts = unpack("H", self.file_handler.read(2))
         self.unique_key_parts = unpack("H", self.file_handler.read(2))
 
-        self.keys = unpack("b", self.file_handler.read(1))
+        self.header_state['keys'] = unpack("b", self.file_handler.read(1))[0]
         self.uniques = unpack("b", self.file_handler.read(1))
         self.languages = unpack("b", self.file_handler.read(1))
         self.max_block_size = unpack("b", self.file_handler.read(1))
-        self.full_text_keys = unpack("b", self.file_handler.read(1))
+        self.header_state['full_text_keys'] = unpack("b", self.file_handler.read(1))[0]
         self.file_handler.read(1) # There is an unused byte here to get to 4 byte alignment
 
         self.state_open_count = unpack("H", self.file_handler.read(2))
@@ -54,7 +68,7 @@ class MyIsamIndexFile:
         self.state_del = unpack("Q", self.file_handler.read(8))
         self.state_split = unpack("Q", self.file_handler.read(8))
         self.state_dellink = unpack("Q", self.file_handler.read(8))
-        
+
         self.state_key_file_length = unpack("Q", self.file_handler.read(8))
         self.state_data_file_length = unpack("Q", self.file_handler.read(8))
         self.state_empty = unpack("Q", self.file_handler.read(8))
--- mySQLBinaries/dataFiles/tests/tests_myisam_index_file.py
+++ mySQLBinaries/dataFiles/tests/tests_myisam_index_file.py
@@ -4,11 +4,22 @@ from unittest import TestCase
 
 class TestsReadMyIsamIndexFile(TestCase):
     def test_verify(self):
+        """
+        When we open a valid mysql myisam index file
+        And that index file is valid
+        Then we will not raise an exception.
+        """
         MyIsamIndexFile("dataFiles/tests/examples/readme/myisam_table_one.MYI")
 
     def test_parse_header(self):
+        """
+        When we open a valid mysql myisam index file
+        And we attempt to read information from the header
+        We will pull out the right information.
+
+        Note: This test uses a cooked up myisam index file that we assume to be correct.
+        """
         index_file = MyIsamIndexFile("dataFiles/tests/examples/readme/myisam_table_one.MYI")
         index_file.parse_header()
-        self.assertEqual(1, index_file.options)
-        self.assertEqual(0, index_file.keys)
-        self.assertEqual(5, index_file.state_records)
+        self.assertEqual(0, index_file.header_state['keys'])
+        self.assertEqual(0, index_file.header_state['full_text_keys'])
